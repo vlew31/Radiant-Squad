10f322c427c8b375be0322ceb76c5b59213069a6
ZhaoYuLL <58865189+ZhaoYuLL@users.noreply.github.com>
2025-04-12 16:16:58 -0400

    home page h2

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,21 +15,21 @@ export const HomePage = () => {
             
             <div className="features-container">
               <div className="feature-card">
-                <h2 className="feature-title">Secure Cloud Storage</h2>
+                <h2 className="feature-title">Feature 1</h2>
                 <p className="feature-description">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam accusantium repellendus pariatur laborum ab sint rerum, voluptatem quisquam quam. Unde accusantium architecto ipsa id fuga vero quam pariatur repudiandae accusamus? 
                 </p>
               </div>
               
               <div className="feature-card">
-                <h2 className="feature-title">Image Optimization</h2>
+                <h2 className="feature-title">Feature 2</h2>
                 <p className="feature-description">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam accusantium repellendus pariatur laborum ab sint rerum, voluptatem quisquam quam. Unde accusantium architecto ipsa id fuga vero quam pariatur repudiandae accusamus? 
                 </p>
               </div>
               
               <div className="feature-card">
-                <h2 className="feature-title">Easy Management</h2>
+                <h2 className="feature-title">Feature 3</h2>
                 <p className="feature-description">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam accusantium repellendus pariatur laborum ab sint rerum, voluptatem quisquam quam. Unde accusantium architecto ipsa id fuga vero quam pariatur repudiandae accusamus? 
                 </p>
